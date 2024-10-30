102ab8cf1cfae158c875423d0c9d60866f436821
gbindusarika <bindusarika123@gmail.com>
2024-10-30 13:25:34 +0530

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -3,5 +3,5 @@ yum install httpd git -y
 service httpd start
 chkconfig httpd on
 cd /var/www/html
-git clone https://github.com/bindusarika/cloudbots.war .
+git clone https://github.com/gbindusarika/CI-CD_PROJECT/blob/master/webserver.sh
 service httpd restart
